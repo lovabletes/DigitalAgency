18dc5bc3e8d82d17b51ab17788ff0c5add53a5a3
Nitin-Singh-32 <rajpootnitin85@gmail.com>
2026-03-02 17:02:06 +0530

    Tawk

--- app/layout.tsx
+++ app/layout.tsx
@@ -171,6 +171,19 @@ export default function RootLayout({
             gtag('config', 'G-DXCZF3VKS6');
           `}
         </Script>
+        <Script id="tawk-to" strategy="lazyOnload">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/69a573dc6f7a6f1c35439427/1jin4p5e5';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
